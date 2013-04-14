9e60729c7deed2489250b925993ab6c3ca973f3c
Mozeze <mozeze.com@gmail.com>
2013-04-14 14:55:17 +0800

    index

--- index.php
+++ index.php
@@ -1,27 +1,92 @@
 <html> 
-<head><title></title> 
-<meta http-equiv="Content-Type" content="text/html; charset=GBK">
-<script language='javascript' type='text/javascript'> 
-var secs =3; 
-var URL ; 
-function Load(url){ 
-URL =url; 
-for(var i=secs;i>=0;i--) 
-{ 
-window.setTimeout('doUpdate(' + i + ')', (secs-i) * 1000); 
-} 
-} 
-function doUpdate(num) 
-{ 
-document.getElementById('ShowDiv').innerHTML = 'Go Blog, '+num; 
-if(num == 0) { window.location=URL; } 
-} 
-</script> 
+<head><title>Index</title> 
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<style type="text/css">
+body { 
+  background: url(/images/index_source_2013.jpg) no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+
+.indexLogo {
+	margin-left: 10%;
+	height:60%;
+	
+	width: 200px;
+	
+	color: rgba(0, 0, 0, 0.7);
+}
+.middleOne {
+	height:10%;
+	width: 100%;
+	position: relative;
+}
+.middleTitle {
+	margin-left:12%;
+	font-size:32px;
+	width: 200px;
+	float: left;
+	color: white;
+}
+
+.middleText {
+	margin-left:30px;
+	font-size:25px;
+	height:35px;
+	width:130px;
+	float: left;
+	color:white;
+}
+
+.textTile {
+	font-family:TpldKhangXiDictTrial;
+}
+
+.text {
+	font-size:26px;
+	font-family:H-宫书;
+}
+
+.middleText2 {
+	margin-left:30px;
+	font-size:25px;
+	font-family:H-宫书;
+	color:white;
+}
+
+.middleTwo {
+	height:5%;
+	width: 100%;
+	float: left;;
+}
+
+.text2 {
+	margin-left:10px;
+}
+
+</style>
 </head> 
-<body> 
-<div id="ShowDiv"></div> 
-<script language="javascript"> 
-Load("blog"); 
-</script> 
-</body> 
+<body>
+	<div id="logo" class="indexLogo">
+		<img alt="www.mozeze.com" src="/images/logo.png" style="margin-top: 30%;margin-left: 10%;">
+	</div>
+	<div id="middleOne" class="middleOne">
+		<div class="middleTitle textTile">袖里青蛇</div>
+		<div class="middleText textTile" style="margin-left: 20%;">白姑娘</div>
+		<div class="middleText textTile">不老歌</div>
+		<div class="middleText textTile" >大象<span class="text">岛</span></div>
+		<div class="middleText text">茉泽映画</div>
+		<div class="middleText text" style="width: 200px;">浮在空中的鱼群</div>
+	</div>
+	<div id="middleTwo" class="middleTwo">
+		<span class="middleTitle">Weibo</span>
+		<span class="middleText" style="margin-left: 21%;">Haku</span>
+		<span class="middleText">Blog</span>
+		<span class="middleText">Draw</span>
+		<span class="middleText">Moze</span>
+		<span class="middleText">Fish</span>
+	</div>
+</body>
 </html>
